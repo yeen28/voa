1245842738e38b4002e8927d6ceafe6489f5ad5b
yeen28 <yestar28@gmail.com>
2023-05-08 11:20:11 +0900

    init

---
+++ src/main/java/com/project/voa/VoaApplication.java
@@ -0,0 +1,13 @@
+package com.project.voa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoaApplication.class, args);
+	}
+
+}

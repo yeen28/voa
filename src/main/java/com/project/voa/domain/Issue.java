03e2301e1f26dbd330b859392fc55f07c96a8bf2
yeen28 <yestar28@gmail.com>
2023-05-13 19:35:35 +0900

    task/MVC 기본 틀 작업 #4
    [작업 내용]
    - 컬럼명을 수정하였습니다.
    - 불필요한 어노테이션을 제거하였습니다.

--- src/main/java/com/project/voa/domain/Issue.java
+++ src/main/java/com/project/voa/domain/Issue.java
@@ -3,13 +3,11 @@ package com.project.voa.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.List;
 
 @Entity
 @Setter
-@EntityListeners(AuditingEntityListener.class)
 public class Issue extends BaseTimeEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/project/voa/domain/UserInfo.java
+++ src/main/java/com/project/voa/domain/UserInfo.java
@@ -1,18 +1,21 @@
 package com.project.voa.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
 public class UserInfo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	@Column(nullable = false)
-	private String user_email;
+	private String userEmail;
 
 	@Column(nullable = false)
-	private String user_name;
+	private String userName;
 
 	@Column(nullable = false)
 	private String password;

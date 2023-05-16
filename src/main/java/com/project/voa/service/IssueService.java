2ad06715c5852a2f94b34ac8e5372be71c74a018
yeen28 <yestar28@gmail.com>
2023-05-16 21:35:33 +0900

    task/사용자에게 할당된 이슈들 가져오는 API #20
    [작업 내용]
    - owner기반으로 할당된 이슈들 조회

--- src/main/java/com/project/voa/service/IssueService.java
+++ src/main/java/com/project/voa/service/IssueService.java
@@ -7,6 +7,8 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class IssueService {
@@ -50,5 +52,14 @@ public class IssueService {
 		// TODO 파라미터 하드코딩
 		Issue issue = issueRepository.findById(id).orElseThrow(EntityNotFoundException::new);
 		issueRepository.delete(issue);
+	}
+
+	/**
+	 * 사용자에게 할당된 이슈들 조회
+	 * @param ownerId
+	 * @return
+	 */
+	public List<Issue> getIssues(final long ownerId) {
+		return issueRepository.findByOwnerId(ownerId);
 	}
 }

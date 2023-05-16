075ae977f55551a466885c1fed4b10407772b57f
yeen28 <yestar28@gmail.com>
2023-05-16 21:35:33 +0900

    task/사용자에게 할당된 이슈들 가져오는 API #20
    [작업 내용]
    - owner기반으로 할당된 이슈들 조회

--- src/main/java/com/project/voa/repository/IssueRepository.java
+++ src/main/java/com/project/voa/repository/IssueRepository.java
@@ -3,5 +3,8 @@ package com.project.voa.repository;
 import com.project.voa.domain.Issue;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface IssueRepository extends CrudRepository<Issue, Long> {
+	List<Issue> findByOwnerId(long ownerId);
 }

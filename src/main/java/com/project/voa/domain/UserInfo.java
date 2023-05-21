d20e940f17bb3171eaa80abccefad45f8734e195
yeen28 <yestar28@gmail.com>
2023-05-21 11:49:55 +0900

    task/테이블_유니크_키_및_인덱스_추가_#24
    [작업 내용]
    - 조회할 양이 많을 테이블에 인덱스 추가
    -- Issue
    -- Label
    -- Version
    -- UserInfo
    - 유니크 키 추가
    -- IssueType의 name
    -- UserInfo의 email

--- src/main/java/com/project/voa/domain/UserInfo.java
+++ src/main/java/com/project/voa/domain/UserInfo.java
@@ -6,12 +6,13 @@ import lombok.*;
 @Entity
 @Getter
 @Setter
+@Table(name = "user_info", indexes = {@Index(name = "user_info_id_index", columnList = "id")})
 public class UserInfo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String userEmail;
 
 	@Column(nullable = false)

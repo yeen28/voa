a1b3df068487d3fccd8a9662b381d324b34e5d62
yeen28 <yestar28@gmail.com>
2023-06-18 17:17:52 +0900

    task/이슈 생성할 때 파일 첨부 할 수 있는 업로드 API 구현 #12
    [작업 내용]
    - 다운로드할 때 id를 전달받기 위해, 파일 첨부 시 반환하는 모델에 첨부파일id 추가

--- src/main/java/com/project/voa/dto/AttachmentModel.java
+++ src/main/java/com/project/voa/dto/AttachmentModel.java
@@ -7,11 +7,13 @@ import lombok.Getter;
 @Getter
 @Builder
 public class AttachmentModel {
+	private String id;
 	private String name;
 	private String createdAt;
 
 	public static AttachmentModel of(Attachment attachment) {
 		return AttachmentModel.builder()
+				.id(String.valueOf(attachment.getId()))
 				.name(attachment.getName())
 				.createdAt(String.valueOf(attachment.getCreatedAt()))
 				.build();

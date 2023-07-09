4a69fedc545c1bfdb07ba87307fe5f1a34d80a78
yeen28 <yestar28@gmail.com>
2023-07-09 11:16:59 +0900

    task/팀 엔티티 CRUD API 생성 #46
    [작업 내용]
    - 팀 엔티티 CRUD 생성
    - team과 admin 데이터를 자동으로 insert하는 쿼리 추가

---
+++ src/main/java/com/project/voa/repository/TeamRepository.java
@@ -0,0 +1,7 @@
+package com.project.voa.repository;
+
+import com.project.voa.domain.Team;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeamRepository extends CrudRepository<Team, Long> {
+}

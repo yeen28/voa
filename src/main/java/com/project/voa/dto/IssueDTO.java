d3236bf237e8b78d3db14800cc8f61f1bf0c499a
yeen28 <yestar28@gmail.com>
2023-07-21 21:06:06 +0900

    task/NotNull 추가
    [작업 내용]
    - NotNull을 NotEmpty 어노테이션으로 수정
    - long 타입은 String과 달라서 empty, blank 체크가 안된다고 합니다.
    - validation으로 검증하는 테스트코드 생성

--- src/main/java/com/project/voa/dto/IssueDTO.java
+++ src/main/java/com/project/voa/dto/IssueDTO.java
@@ -1,7 +1,7 @@
 package com.project.voa.dto;
 
 import com.project.voa.domain.IssueStatus;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,14 +11,11 @@ import java.util.List;
 @Setter
 public class IssueDTO {
 	private long issueTypeId;
-	@NotNull(message = "TITLE_IS_NULL")
+	@NotEmpty(message = "TITLE_IS_NULL")
 	private String title;
-	@NotNull(message = "RANK_IS_NULL")
 	private long rank;
 	private List<String> versionNames;
-	@NotNull(message = "OWNER_IS_NULL")
 	private long ownerId;
-	@NotNull(message = "REPORTER_IS_NULL")
 	private long reporterId;
 	private String env;
 	private String description;

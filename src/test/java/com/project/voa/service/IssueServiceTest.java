9f53242e8c7d967a99d40a9001716623aae46bbc
yeen28 <yestar28@gmail.com>
2023-08-11 22:55:39 +0900

    task/테스트코드 추가 #69
    [작업 내용]
    - IssueService에 대한 테스트코드 추가

--- src/test/java/com/project/voa/service/IssueServiceTest.java
+++ src/test/java/com/project/voa/service/IssueServiceTest.java
@@ -24,30 +24,107 @@ class IssueServiceTest {
 	@Mock IssueRepository issueRepository;
 	@InjectMocks IssueService issueService;
 
-	@Test
-	void createIssueTest() {
-		// given
-		IssueType issueType = new IssueType();
-		Version version = new Version();
-		Label label = new Label();
-		UserInfo userInfo = new UserInfo();
+	private Issue issueSetup() {
+		IssueType issueType = mock(IssueType.class);
+		issueType.setName("issueType");
+		Version version = mock(Version.class);
+		Label label = mock(Label.class);
+		UserInfo userInfo = mock(UserInfo.class);
+		Attachment attachment = mock(Attachment.class);
 
+		return Issue.builder()
+				.id(1L)
+				.title("title")
+				.issueType(issueType)
+				.rank(1L)
+				.versions(List.of(version))
+				.owner(userInfo)
+				.reporter(userInfo)
+				.env("environment")
+				.description("description")
+				.labels(List.of(label))
+				.issueLinkType(1L)
+				.issueLink("issueLink")
+				.issueStatus(IssueStatus.TO_DO)
+				.attachments(List.of(attachment))
+				.build();
+	}
+
+	private IssueDTO issueDtoSetup() {
 		IssueDTO issueDTO = new IssueDTO();
+		issueDTO.setTitle("issue title");
 		issueDTO.setIssueTypeId(1L);
 		issueDTO.setVersionNames(List.of("version"));
 		issueDTO.setOwnerId(1L);
 		issueDTO.setReporterId(1L);
 		issueDTO.setLabelNames(List.of("label"));
 
+		return issueDTO;
+	}
+
+	@Test
+	void createIssueTest() {
+		IssueType issueType = new IssueType();
+		Version version = new Version();
+		Label label = new Label();
+		UserInfo userInfo = new UserInfo();
+		IssueDTO issueDTO = issueDtoSetup();
+
 		when(issueTypeRepository.findById(any())).thenReturn(Optional.of(issueType));
 		when(userInfoRepository.findById(any())).thenReturn(Optional.of(userInfo));
 		when(versionRepository.findByName(anyString())).thenReturn(Optional.of(version));
 		when(labelRepository.findByName(anyString())).thenReturn(Optional.of(label));
 
-		//when
 		issueService.createIssue(issueDTO);
 
-		//then
-		verify(issueRepository, atLeastOnce()).save(any(Issue.class));
+		verify(issueRepository, times(1)).save(any(Issue.class));
+	}
+
+	@Test
+	void getIssueTest() {
+		Issue issue = issueSetup();
+
+		doReturn(Optional.of(issue)).when(issueRepository).findById(anyLong());
+
+		issueService.getIssue(1L);
+
+		verify(issueRepository, times(1)).findById(1L);
+	}
+
+	@Test
+	void updateIssueStatusTest() {
+		Issue issue = issueSetup();
+		doReturn(Optional.of(issue)).when(issueRepository).findById(anyLong());
+
+		issueService.updateIssueStatus(1L, IssueStatus.TO_DO);
+
+		verify(issueRepository, times(1)).save(issue);
+	}
+
+	@Test
+	void updateIssueTest() {
+		Issue issue = issueSetup();
+		IssueType issueType = issue.getIssueType();
+		UserInfo userInfo = issue.getOwner();
+		IssueDTO issueDTO = issueDtoSetup();
+
+		doReturn(Optional.of(issue)).when(issueRepository).findById(anyLong());
+		doReturn(Optional.of(issueType)).when(issueTypeRepository).findById(anyLong());
+		doReturn(Optional.of(userInfo)).when(userInfoRepository).findById(anyLong());
+		doReturn(issue).when(issueRepository).save(issue);
+
+		issueService.updateIssue(1L, issueDTO);
+
+		verify(issueRepository, times(1)).save(issue);
+	}
+
+	@Test
+	void deleteIssueTest() {
+		Issue issue = issueSetup();
+		doReturn(Optional.of(issue)).when(issueRepository).findById(anyLong());
+
+		issueService.deleteIssue(1L);
+
+		verify(issueRepository, times(1)).delete(issue);
 	}
 }

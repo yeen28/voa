061c837076b2303a02f261f7795311098a964943
yeen28 <yestar28@gmail.com>
2024-01-27 22:12:56 +0900

    task/svelte환경구성 [task] - proxy로 서버와 연결 - GET "/main"으로 호출해야 svelte로 구성된 화면으로 노출됩니다. [trouble shooting] - '... $$ ...'와 같은 오류가 발생하지 않도록 하기 위해서 "svelte": "^3.39.0" 버전으로 낮췄습니다. - main.html에서 dist를 참조할 때 꼭 defer를 추가해야 svelte 컴포넌트를 사용할 수 있습니다.

---
+++ front-voa/README.md
@@ -0,0 +1,18 @@
+# VOA Frontend
+
+## How to setup?
+```
+1. Build gradle script after you clone this project
+2. Install node.js if not insatalled then reboot computer
+3. Run command "npm install" in terminal 
+```
+
+## How to run?
+```
+1. Start spring boot
+2. cd front-voa
+3. Run command "npm run build" or 
+   Run command "npm run dev" in terminal for watch
+4. After finished the build, re-run spring boot application
+5. Refresh web browser - sometimes you need hard refresh
+```
---
+++ front-voa/rollup.config.js
@@ -0,0 +1,48 @@
+import svelte from 'rollup-plugin-svelte';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import livereload from 'rollup-plugin-livereload';
+import css from 'rollup-plugin-css-only';
+
+const production = !process.env.ROLLUP_WATCH;
+
+const entries = [
+	'main'
+];
+
+export default entries.map( entryName =>
+{
+	return {
+		input: 'src/main.js',
+		output: {
+			sourcemap: true,
+			format: 'iife',
+			name: 'app',
+			// TODO 추후 svelte 빌드 결과물을 springboot 실행 시 자동 빌드를 위해 springboot의 static 디렉토리로 위치시킴.
+			// file: `../src/main/resources/static/dist/${entryName}/bundle.js`
+			file: `../src/main/resources/static/dist/bundle.js`
+		},
+		plugins: [
+			commonjs(),
+			css(),
+			svelte({
+				dev: !production,
+				css: css => {
+					css.write('public/build/bundle.css');
+				}
+			}),
+			resolve({
+				browser: true,
+				dedupe: ['svelte']
+			}),
+			!production && livereload('public'),
+			production && proxy({
+				// API 서버 주소로 변경
+				targets: [{src: '/api/.*', dest: 'http://localhost:8080'}]
+			})
+		],
+		watch: {
+			clearScreen: false
+		}
+	}
+});

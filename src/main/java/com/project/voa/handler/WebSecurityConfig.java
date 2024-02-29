54b00f4c7518ebd9a404df7df73868601f68f717
yeen28 <yestar28@gmail.com>
2024-02-29 13:05:14 +0900

    task/add_JWT [task] - 로그인이 필요한 controller에 인증없이 접속하는 경우 로그인 페이지로 이동하도록 securityFilterChain에 핸들링 추가

--- src/main/java/com/project/voa/handler/WebSecurityConfig.java
+++ src/main/java/com/project/voa/handler/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.project.voa.handler;
 
 import com.project.voa.jwt.JwtSecurityConfig;
 import com.project.voa.jwt.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +13,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import java.io.IOException;
 
 /**
  * Spring Security 환경 설정을 구성하기 위한 클래스입니다.
@@ -38,6 +43,17 @@ public class WebSecurityConfig {
 		httpSecurity
 				// token을 사용하는 방식이기 때문에 csrf를 disable합니다.
 				.csrf(AbstractHttpConfigurer::disable)
+
+				// 인증 실패와 권한 실패 예외 핸들링
+				.exceptionHandling(exception -> {
+					exception.authenticationEntryPoint(new Http403ForbiddenEntryPoint() {
+						@Override
+						public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+							response.sendRedirect("/login");
+						}
+					});
+					exception.accessDeniedHandler((request, response, accessDeniedException) -> log.warn("권한이 없습니다"));
+				})
 
 				.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
 						.requestMatchers("/**").permitAll()

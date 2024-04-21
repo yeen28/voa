e9d3655497fa3190ba3621178c27ec4ebea2a9bd
rerewww <rerewww@naver.com>
2024-04-22 00:16:41 +0900

    task/-/UserInfoService 테스트 코드 작성 - Create, Read, Delete 테스트 코드 작성

--- src/main/java/com/project/voa/dto/VersionModel.java
+++ src/main/java/com/project/voa/dto/VersionModel.java
@@ -22,4 +22,9 @@ public class VersionModel {
 	public static List<VersionModel> versionsToVersionModels(List<Version> versions) {
 		return versions.stream().map(VersionModel::of).toList();
 	}
+
+	@Override
+	public String toString() {
+		return String.format("id: %s, name: %s", this.id, this.name);
+	}
 }

c3bd51bd91f76f8c0b54c0436d937910669efd1b
yeen28 <yestar28@gmail.com>
2024-09-17 23:30:39 +0900

    task/-/Implementing issue showing type change to svelte [Task] - issue 보여주는 타입을 svelte로 구현

--- src/main/java/com/project/voa/controller/IssueController.java
+++ src/main/java/com/project/voa/controller/IssueController.java
@@ -30,8 +30,8 @@ public class IssueController {
 
 	@PreAuthorize("hasRole('ROLE_USER')")
 	@Operation(summary = "사용자에게 할당된 이슈 조회")
-	@GetMapping("/issues")
-	public ResponseEntity<Object> getIssues(@RequestParam("ownerId") final long ownerId) {
+	@GetMapping("/issues/user/{ownerId}")
+	public ResponseEntity<Object> getIssues(@PathVariable("ownerId") final long ownerId) {
 		return new ResponseEntity<>(issueService.getIssues(ownerId), HttpStatus.OK);
 	}
 

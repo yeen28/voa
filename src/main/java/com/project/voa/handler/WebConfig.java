683f5a5ed73062aff74e4c513c7ff1dc1fef1d85
yeen28 <yestar28@gmail.com>
2024-09-18 13:39:16 +0900

    task/-/Add UserInfoArgumentResolver [Task] - UserInfoArgumentResolver 구현 - Spring Security 환경이기 때문에 인증된 사용자 정보를 가져오는 SecurityContextHolder를 사용 - UserInfoArgumentResolver는 UserInfoRepository가 필요하므로 Spring에서 의존성을 주입받을 수 있게 Spring Bean으로 등록 - WebConfig에서 직접 new 키워드를 사용하지 않고, Spring Bean을 주입받아 addArgumentResolvers에 추가

--- src/main/java/com/project/voa/handler/WebConfig.java
+++ src/main/java/com/project/voa/handler/WebConfig.java
@@ -1,5 +1,6 @@
 package com.project.voa.handler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -13,9 +14,16 @@ import java.util.List;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	private final UserInfoArgumentResolver userInfoArgumentResolver;
+
+	@Autowired
+	public WebConfig(UserInfoArgumentResolver userInfoArgumentResolver) {
+		this.userInfoArgumentResolver = userInfoArgumentResolver;
+	}
+
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-		resolvers.add(new UserInfoArgumentResolver());
+		resolvers.add(userInfoArgumentResolver);
 	}
 
 	@Override

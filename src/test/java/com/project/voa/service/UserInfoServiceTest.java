884b99f3f74f6ba82685ad853c73d09b633969d9
yeen28 <yestar28@gmail.com>
2024-09-21 04:48:10 +0900

    task/-/Add test database [Task] - 실제 운영 환경과 테스트 환경을 명확하게 분리함으로써, 테스트의 안정성과 신뢰성을 높이고 실수로 운영 데이터를 손상시키는 위험을 줄이기 위해 테스트용 데이터베이스 추가 -- 인메모리 데이터베이스(h2) 사용 - application-test.yml을 추가하여 테스트용 데이터베이스 설정 추가 - @ActiveProfiles("test") 어노테이션을 사용하여 테스트 환경에서만 테스트 프로파일을 활성화

--- src/test/java/com/project/voa/service/UserInfoServiceTest.java
+++ src/test/java/com/project/voa/service/UserInfoServiceTest.java
@@ -4,12 +4,14 @@ import com.project.voa.dto.VersionModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserInfoServiceTest {
     @Autowired VersionService versionService;
 
